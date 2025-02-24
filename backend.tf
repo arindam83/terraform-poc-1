9069f8ad0c54194c536826f6d5428aeae3aae611
arindam83 <ahalder.bkp@gmail.com>
2025-02-23 19:08:56 -0500

    cmt

--- backend.tf
+++ backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"  
     organization = "arindam" 
- workspace = "my-tfagent-ws-01"
+  workspaces {
+      name = "my-tfagent-ws-01"
+    }
   }
 }
