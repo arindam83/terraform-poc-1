f55ed78bd0a1322c38449dbb722c5e6973ca7419
arindam83 <ahalder.bkp@gmail.com>
2025-02-23 19:46:03 -0500

    cmt

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
    tags = {
     Environment = "POC"
-    Owner       = "Arindam"
+    Owner       = "Arindam halder"
   }
 }
 #new rg  creation
